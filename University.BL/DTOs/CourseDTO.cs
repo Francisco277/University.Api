b16d647c95ff0dccfcc6ff567c34e0b07aacab8d
= <francisco.trochez.0ds@utap.edu.co>
2021-09-13 21:41:58 -0500

    Agregar archivos de proyecto.

---
+++ University.BL/DTOs/CourseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.BL.DTOs
+{
+  public  class CourseDTO
+    {
+        [Required(ErrorMessage = "The ID es Required")]
+        public int CourseID { get; set; }
+
+        [Required(ErrorMessage = "The Title es Required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Credits es Required")]
+
+        public int Credits { get; set; }
+    }
+}
