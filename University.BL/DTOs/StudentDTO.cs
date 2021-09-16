d5b2c0a738abc54dfbc5c827e5a5bd9a96db579a
= <francisco.trochez.0ds@utap.edu.co>
2021-09-15 21:30:21 -0500

    Crud Students api

--- University.BL/DTOs/StudentDTO.cs
+++ University.BL/DTOs/StudentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace University.BL.DTOs
 {
     public class StudentDTO
     {
+
         public int ID { get; set; }
+        [Required(ErrorMessage ="The Fiel Last Name is required")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "The Fiel First Name is required")]
         public string FirstMidName { get; set; }
+        [Required(ErrorMessage = "The Fiel EnrollmenDate is required")]
         public DateTime EnrollmentDate { get; set; }
         public string FullName
         {
